43a4df37237fefee7cc750827fd599d8e0d6bc26
Fjx <917756350@qq.com>
2018-12-06 20:22:55 +0800

    第一次在idea使用git

---
+++ mybatis/src/com/itheima/GitDemo.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+/**
+ * @author FJx
+ * @date 2018/12/6 20:02
+ */
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
