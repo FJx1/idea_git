2b927017170949c62a65f16972d408323026f01a
Fjx <917756350@qq.com>
2018-12-06 20:51:48 +0800

    从idea到本地仓库

---
+++ gitText/src/com/itheima/demo.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+/**
+ * @author FJx
+ * @date 2018/12/6 20:47
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("demo...");
+    }
+}
